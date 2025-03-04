91f04047798ad66f66c99d4b818baa90fe5a7998
Kacie <C00299407@setu.ie>
2025-03-04 12:25:34 +0000

    Added Kacie pages

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -3,3 +3,6 @@
 <a href="/about">About Page</a>
 <a href="/dynamic">Main Page</a>
 <a href="/404-error">404 Error Page</a>
+<a href="/contact">Contact Page</a>
+<a href="/legal-info">Terms of Service Page</a>
+<a href="/products">Products Page</a>
