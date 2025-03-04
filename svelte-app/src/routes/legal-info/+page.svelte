d548c71daa70786b18217268b2261a4f6222fefa
Kacie <C00299407@setu.ie>
2025-03-04 14:34:00 +0000

    TOS style

--- svelte-app/src/routes/legal-info/+page.svelte
+++ svelte-app/src/routes/legal-info/+page.svelte
@@ -1,4 +1,6 @@
 <h1>This is the Terms of Service Page</h1>
+<div id="tos-text">
 <h2>Terms of Service</h2>
-<p>By using this website, you agree to forfeit your rights to reproduction of images created via this website<br></p>
+<p>By using this website, you agree to forfeit your rights to reproduction of images created via this website.<br></p>
 <a href="/" class="form-submit">Accept</a>
+</div>
