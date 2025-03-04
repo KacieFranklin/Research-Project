a4b0c9e4d1adec2572180de170531d4ad66595b3
CialanJPatterson <c00297034@setu.ie>
2025-03-04 23:44:32 +0000

    added links to nav

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -2,17 +2,15 @@
     let { children } = $props();
 </script>
 
-<header>
-    <nav>
-        <span><a href="/">Home</a></span>
-        <span><a href="/">Home</a></span>
-        <span><a href="/">Contact Us</a></span>
-        <span><a href="/">About</a></span>
-        <span><a href="/">News</a></span>
-        <span><a href="/">Products</a></span>
-        <span><a href="/">Questions</a></span>
-        <span><a href="/">Questions</a></span>
-    </nav>
-</header>
+<nav>
+    <span><a href="/">Home</a></span>
+    <span><a href="/about">About</a></span>
+    <span><a href="/contact">Contact Us</a></span>
+    <span><a href="/legal-info">Terms of Use</a></span>
+    <span><a href="/news">Blog/News</a></span>
+    <span><a href="/products">Products and Services</a></span>
+    <span><a href="/questions">FAQ</a></span>
+    <span><a href="/404-error">4̷͈̚0̶̩̫͂͝4̶̯̽̈́ͅ-̷̛͎̃p̸͔͒ḁ̴̢̉̋g̵͔̏e̵͙̣̽̐</a></span>
+</nav>
 
 {@render children()}
