84075021219a9feb3bd5ea814f0134d070761cb9
Kacie <C00299407@setu.ie>
2025-03-04 15:05:48 +0000

    040325 Incomplete Div in +layout.svelte

---
+++ svelte-app/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    let { children } = $props();
+</script>
+
+<header>
+    <nav>
+        <div class="link"><a href="/">Home</a></div>
+        <div class="link"><a href="/">Home</a></div>
+            <li><a href="/">Contact Us</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">News</a></li>
+            <li><a href="/">Products</a></li>
+            <li><a href="/">Questions</a></li>
+    </nav>
+</header>
+
+{@render children()}
