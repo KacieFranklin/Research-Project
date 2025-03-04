141c756940cad56f9ece7f8d5c2cbc4cfa8d1621
Kacie <C00299407@setu.ie>
2025-03-04 14:11:00 +0000

    Added text to legal

--- svelte-app/src/routes/legal-info/+page.svelte
+++ svelte-app/src/routes/legal-info/+page.svelte
@@ -1,3 +1,4 @@
 <h1>This is the Terms of Service Page</h1>
-
+<h2>Terms of Service</h2>
+<p>By using this website, you agree to forfeit your rights to reproduction of images created via this website<br></p>
 <a href="/" class="form-submit">Accept</a>
