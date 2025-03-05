e8f9274e80e49d9ee0a84b0fb4ed7b12d08fe7b7
CialanJPatterson <c00297034@setu.ie>
2025-03-05 02:48:47 +0000

    added scrolling div and formatted homepage

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -4,16 +4,17 @@
 
 <div id="page-content">
     <h1>Homepage</h1>
-    <a href="/about">About Page</a><br>
-    <a href="/dynamic">Main Page</a><br>
-    <a href="/404-error">404 Error Page</a><br>
-    <a href="/contact">Contact Page</a><br>
-    <a href="/legal-info">Terms of Service Page</a><br>
-    <a href="/products">Products Page</a><br>
-    <a href="/questions">FAQ Page</a><br>
-    <a href="/news">Blog/News Page</a><br>
+    <h2>Create Amazing Digital Effects</h2>
+    <p>With our unique parallax technology you can create epic looking images for your website.<br></p>
+    <h2>How Does it Work?</h2>
+    <p>Visit our <a href="/questions">FAQ Page</a> to learn how the tool works<br></p>
+    <p>And visit our <a href="/about">About section</a> to learn about us too<br></p>
+    <h1><br><a href="/dynamic">Start Drawing!</a><br></h1>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <p>there's text down here</p>
 
-    <div style="position:fixed;bottom:0;background-color:#3365">
+    <div style="position:fixed;bottom:0;background-color:#446">
         <a href="https://svelte.dev/docs/kit">Svelte Documentation</a><br>
     </div>
 </div>
