3fcc3b281b00c7c1600b867b8bfbec63fa2363f0
kacie franklin <C00299407@setu.ie>
2025-03-05 00:04:50 +0000

    Added text to products page

--- svelte-app/src/routes/products/+page.svelte
+++ svelte-app/src/routes/products/+page.svelte
@@ -1,2 +1,3 @@
 <h1>This is the Products Page</h1>
+<p>Below shows the key binds for each tool associated with the drawing tool. Feel free to change them to fit your preferences!</p>
 <a href="/">Home</a><br>
