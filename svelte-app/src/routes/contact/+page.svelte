4d315bc7209007d2c6674a48e1d20e79199c3bff
Kacie <C00299407@setu.ie>
2025-03-11 12:08:11 +0000

    Scroll and email added to contact and scroll added to product

--- svelte-app/src/routes/contact/+page.svelte
+++ svelte-app/src/routes/contact/+page.svelte
@@ -4,6 +4,18 @@
 
 <div id="page-content">
     <h1>This is the Contact page</h1>
-    <p>If you would like to contact us, below are how you can reach us.</p>
+    <p>If you would like to contact us, below is how you can get in touch with us for any inquires or issues you may be having.</p>
+    <p><a href="mailto:C00299407@setu.ie"> Kacie Franklin: C00299407@setu.ie</a></p><br>
+    <p><a href="mailto:C00297034@setu.ie"> Cialan Patterson: C00297034@setu.ie</a></p><br>
+    <p><a href="mailto:C00297358@setu.ie"> Robert Iova: C00297358@setu.ie</a></p><br>
     <a href="/">Home</a><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <p>there's text down here</p>
+
+    <div style="position:fixed;bottom:0;background-color:#446">
+        <a href="https://svelte.dev/docs/kit">Svelte Documentation</a><br>
+    </div>
 </div>
+ 
--- svelte-app/src/routes/products/+page.svelte
+++ svelte-app/src/routes/products/+page.svelte
@@ -6,4 +6,14 @@
     <h1>This is the Products Page</h1>
     <p>Below shows the key binds for each tool associated with the drawing tool. Feel free to change them to fit your preferences!</p>
     <a href="/">Home</a><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <p>there's text down here</p>
+
+    <div style="position:fixed;bottom:0;background-color:#446">
+        <a href="https://svelte.dev/docs/kit">Svelte Documentation</a><br>
+    </div>
 </div>
+ 
