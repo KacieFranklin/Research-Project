042c235f51b41ff1d31c6a42a414f57873f0537e
Kacie <C00299407@setu.ie>
2025-03-18 14:47:17 +0000

    Refactored Footer

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { Navigation } from "$lib";
+    import { Navigation, Footer } from "$lib";
     let { children } = $props();
 </script>
 
@@ -7,14 +7,11 @@
     <Navigation />
 </header>
 
+<Footer />
+
 <!-- svelte-ignore a11y_invalid_attribute -->
 <a href="javascript:window.history.back()">Go Back</a>
 
 {@render children()}
 
-<footer>
-    <div style="position:fixed;bottom:45px;background-color:#446;z-index:8">
-        <a href="https://svelte.dev/docs/kit">Svelte Documentation</a><br>
-    </div>
-    <p>2025 Art Website. All rights reserved</p>
-</footer>
+
