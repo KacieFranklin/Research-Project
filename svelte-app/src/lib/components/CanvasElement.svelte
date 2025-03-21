678380da31d9656749d4266219ea63d98cd1b19b
CialanJPatterson <c00297034@setu.ie>
2025-03-21 12:53:02 +0000

    added stub component CanvasElement

---
+++ svelte-app/src/lib/components/CanvasElement.svelte
@@ -0,0 +1,24 @@
+<script module>
+    class layer_data {
+        /**
+         * @param image {Image}
+         * @param depth {Number}
+         * @param height {Number}
+         * @param width {Number}
+         * @param defaultX {Number}
+         * @param defaultY {Number}
+         */
+        constructor(image,depth,height,width,defaultX,defaultY){
+            this.data = image;
+            this.depth = depth;
+            this.h = height * depth;
+            this.w = width * depth;
+            this.x = defaultX - (this.w - width) / 2;
+            this.y = defaultY - (this.h - height) / 2;
+        }
+
+        calcParallax() {
+
+        }
+    }
+</script>
--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -14,18 +14,33 @@
 
 <script>
     function onload() {
+        localStorage.setItem("stored_data", "first"); //TEST
         if (localStorage.getItem("stored_data") == null) {
             window.location.href = "/legal-info";
-            localStorage.setItem("stored_data", "True");
+            localStorage.setItem("stored_data", "first");
+        }
+        else if (localStorage.getItem("stored_data") == "first") {
+            //localStorage.setItem("stored_data", "returning"); //TEST
+        }
+        else {
+            window.location.href = "/dynamic";
         }
     }
     function clearData() {
         localStorage.removeItem("stored_data");
     }
     onload();
+
+    import { CanvasElement } from "$lib";
+    let c;
 </script>
 
 <h1><br><a href="/dynamic">Start Drawing!</a><br></h1>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br>
+<canvas bind:this={c}></canvas>
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
 <p>there's text down here</p>
