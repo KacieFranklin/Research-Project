021e1e64bcc5c284fe441284b27f885587e570d3
Kacie <C00299407@setu.ie>
2025-03-21 11:48:12 +0000

    Hambuger menu sticky

--- svelte-app/src/lib/components/Navigation.svelte
+++ svelte-app/src/lib/components/Navigation.svelte
@@ -10,17 +10,17 @@
     <button class="burger" on:click={toggleHamburger} aria-label="Toggle navigation">
         ≡
     </button>
-</nav>
 
-<div class="hamburger-menu">
-    <ul class:open={isOpen}>
-        <span><a href="/">Home</a></span>
-        <span><a href="/about">About</a></span>
-        <span><a href="/contact">Contact Us</a></span>
-        <span><a href="/legal-info">Terms of Use</a></span>
-        <span><a href="/news">Blog/News</a></span>
-        <span><a href="/products">Products and Services</a></span>
-        <span><a href="/questions">FAQ</a></span>
-        <span><a href="/404-error">4̷͈̚0̶̩̫͂͝4̶̯̽̈́ͅ-̷̛͎̃p̸͔͒ḁ̴̢̉̋g̵͔̏e̵͙̣̽̐</a></span>
-    </ul>
-</div>
+    <div class="hamburger-menu">
+        <ul class:open={isOpen}>
+            <span><a href="/">Home</a></span>
+            <span><a href="/about">About</a></span>
+            <span><a href="/contact">Contact Us</a></span>
+            <span><a href="/legal-info">Terms of Use</a></span>
+            <span><a href="/news">Blog/News</a></span>
+            <span><a href="/products">Products and Services</a></span>
+            <span><a href="/questions">FAQ</a></span>
+            <span><a href="/404-error">4̷͈̚0̶̩̫͂͝4̶̯̽̈́ͅ-̷̛͎̃p̸͔͒ḁ̴̢̉̋g̵͔̏e̵͙̣̽̐</a></span>
+        </ul>
+    </div>
+</nav>
