83379b115f326cb1d0ac94b4e6c2ed3c0c516617
Kacie <C00299407@setu.ie>
2025-03-28 12:57:38 +0000

    Started formatting products page with imgs

--- svelte-app/src/routes/products/+page.svelte
+++ svelte-app/src/routes/products/+page.svelte
@@ -5,6 +5,17 @@
 <h1>This is the Products Page</h1>
 <p>Below shows the key binds for each tool. Feel free to change them to fit your preferences when using the Drawing Tool!</p>
 <a href="/home">Home</a><br>
+
+<div class="icon-imgs">
+</div>
+
+<div class="brush-img"><img src="brush.png" alt="Brush tool"></div>
+<div class="eraser-img"><img src="eraser.png" alt="Eraser tool"></div>
+<div class="lasso-img"><img src="lasso.png" alt="Lasso tool"></div>
+<div class="drag-img"><img src="drag.png" alt="Drag tool"></div>
+<div class="cut-img"> <img src="cut.png" alt="Cut tool"></div>
+ 
+
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
