d4fb648802bc534ecc9763e74883eb764cb55aab
Kacie <C00299407@setu.ie>
2025-04-04 12:53:23 +0100

    Alinged icon imgs on product page

--- svelte-app/src/routes/products/+page.svelte
+++ svelte-app/src/routes/products/+page.svelte
@@ -3,18 +3,31 @@
 </svelte:head>
 
 <h1>This is the Products Page</h1>
-<p>Below shows the key binds for each tool. Feel free to change them to fit your preferences when using the Drawing Tool!</p>
+<p>Below shows the key binds for each tool as well as a small description on what the tools do. Feel free to change them to fit your preferences when using the Drawing Tool!</p>
 <a href="/home">Home</a><br>
 
-<div class="icon-imgs">
-</div>
-
-<div class="brush-img"><img src="brush.png" alt="Brush tool"></div>
-<div class="eraser-img"><img src="eraser.png" alt="Eraser tool"></div>
-<div class="lasso-img"><img src="lasso.png" alt="Lasso tool"></div>
-<div class="drag-img"><img src="drag.png" alt="Drag tool"></div>
-<div class="cut-img"> <img src="cut.png" alt="Cut tool"></div>
- 
+<ul>
+    <li class="tool-icon">
+        <img  src="brush.png" alt="Brush tool" width="200px" height="220px">
+        <h2 class="tool-shortcut">Brush: Shift + B</h2>
+    </li>
+    <li class="tool-icon">
+        <img src="eraser.png" alt="Eraser tool" width="200px" height="220px">
+        <h2 class="tool-shortcut">Eraser: Shift + E</h2>
+    </li>
+    <li class="tool-icon">
+        <img src="lasso.png" alt="Lasso tool" width="200px" height="220px">
+        <h2 class="tool-shortcut">Lasso: Shift + L</h2>
+    </li>
+    <li class="tool-icon">
+        <img src="drag.png" alt="Drag tool" width="200px" height="220px">
+        <h2 class="tool-shortcut">Drag: Shift + D</h2>
+    </li>
+    <li class="tool-icon">
+        <img src="cut.png" alt="Cut tool" width="200px" height="220px">
+        <h2 class="tool-shortcut">Cut: Shift + C</h2>
+    </li>
+</ul> 
 
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
