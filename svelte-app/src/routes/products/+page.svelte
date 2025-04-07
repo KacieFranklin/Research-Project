ba1c37b9ed250dccaceba10e633857132f154e40
CialanJPatterson <c00297034@setu.ie>
2025-04-07 16:02:21 +0100

    added interactive background to Products page

--- svelte-app/src/routes/products/+page.svelte
+++ svelte-app/src/routes/products/+page.svelte
@@ -6,35 +6,34 @@
 <p>Below shows the key binds for each tool as well as a small description on what the tools do. Feel free to change them to fit your preferences when using the Drawing Tool!</p>
 <a href="/home">Home</a><br>
 
-<ul>
-    <li class="tool-icon">
-        <img  src="brush.png" alt="Brush tool" width="200px" height="220px">
-        <h2 class="tool-shortcut">Brush: Shift + B</h2>
-    </li>
-    <li class="tool-icon">
-        <img src="eraser.png" alt="Eraser tool" width="200px" height="220px">
-        <h2 class="tool-shortcut">Eraser: Shift + E</h2>
-    </li>
-    <li class="tool-icon">
-        <img src="lasso.png" alt="Lasso tool" width="200px" height="220px">
-        <h2 class="tool-shortcut">Lasso: Shift + L</h2>
-    </li>
-    <li class="tool-icon">
-        <img src="drag.png" alt="Drag tool" width="200px" height="220px">
-        <h2 class="tool-shortcut">Drag: Shift + D</h2>
-    </li>
-    <li class="tool-icon">
-        <img src="cut.png" alt="Cut tool" width="200px" height="220px">
-        <h2 class="tool-shortcut">Cut: Shift + C</h2>
-    </li>
-</ul> 
-
-<div class="information-tools" ></div>
-
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<div class="information-tools">
+    <ul class="nested-list">
+        <li class="tool-icon">
+            <img  src="brush.png" alt="Brush tool" width="200px" height="220px">
+            <h2 class="tool-shortcut">Brush: Shift + B</h2>
+        </li>
+        <li class="tool-icon">
+            <img src="eraser.png" alt="Eraser tool" width="200px" height="220px">
+            <h2 class="tool-shortcut">Eraser: Shift + E</h2>
+        </li>
+        <li class="tool-icon">
+            <img src="lasso.png" alt="Lasso tool" width="200px" height="220px">
+            <h2 class="tool-shortcut">Lasso: Shift + L</h2>
+        </li>
+        <li class="tool-icon">
+            <img src="drag.png" alt="Drag tool" width="200px" height="220px">
+            <h2 class="tool-shortcut">Drag: Shift + D</h2>
+        </li>
+        <li class="tool-icon">
+            <img src="cut.png" alt="Cut tool" width="200px" height="220px">
+            <h2 class="tool-shortcut">Cut: Shift + C</h2>
+        </li>
+    </ul>
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br>
 <p>text</p>
-
- 
