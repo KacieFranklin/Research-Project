5d73a41f78e0e8ccd9beb1c2e29052c541cad230
CialanJPatterson <c00297034@setu.ie>
2025-04-07 21:58:41 +0100

    legal page fixed

--- svelte-app/src/routes/legal-info/+page.svelte
+++ svelte-app/src/routes/legal-info/+page.svelte
@@ -11,11 +11,11 @@
 
 <div id="tos-text">
     <h2>Terms of Service</h2>
-    <p>Welcome to Perseplex! These Terms of Service govern your use of our website, you can create and save your art to use however you like.<br></p>
+    <p>Welcome to Perseplax! These Terms of Service govern your use of our website, you can create and save your art to use however you like.<br></p>
     <p><strong>License to Use Content</strong></p>
     <p>When you create art using our website, you retain ownership of your artwork.<br></p>
-    <p><strong>intellectual Property</strong></p>
-    <p>All content on the website, including images, graphics, logos and software, is owned by Perseplex and its developers and is protected by intellectual propert laws.<br></p>
+    <p><strong>Intellectual Property</strong></p>
+    <p>All content on the website, including images, graphics, logos and software, is owned by Perseplax and its developers and is protected by intellectual property laws.<br></p>
     <p><strong>Prohibited Activities</strong></p>
     <p>You must not engage in any activity that could damage, disable or interfere with the proper functioning of the program.<br></p>
     <p><strong>Modifications of Terms</strong></p>
