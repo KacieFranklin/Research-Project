64a5a7296b325a822b44c2eb165988450e0d0c25
CialanJPatterson <c00297034@setu.ie>
2025-04-07 18:55:24 +0100

    Added vertical tool list with global.css

--- svelte-app/src/routes/products/+page.svelte
+++ svelte-app/src/routes/products/+page.svelte
@@ -6,51 +6,59 @@
 <p>Below shows the key binds for each tool as well as a small description on what the tools do. Feel free to change them to fit your preferences when using the Drawing Tool!</p>
 <a href="/home">Home</a><br>
 
-    <ul>
-        <li class="tool-icon">
+<ul class="tool-list">
+    <li>
+        <div class="tool-icon">
             <img  src="brush.png" alt="Brush tool" width="200px" height="220px">
             <h2 class="tool-shortcut">Brush: Shift + B</h2>
-        </li>
-        <li class="tool-icon">
-            <img src="eraser.png" alt="Eraser tool" width="200px" height="220px">
-            <h2 class="tool-shortcut">Eraser: Shift + E</h2>
-        </li>
-        <li class="tool-icon">
-            <img src="lasso.png" alt="Lasso tool" width="200px" height="220px">
-            <h2 class="tool-shortcut">Lasso: Shift + L</h2>
-        </li>
-        <li class="tool-icon">
-            <img src="drag.png" alt="Drag tool" width="200px" height="220px">
-            <h2 class="tool-shortcut">Drag: Shift + D</h2>
-        </li>
-        <li class="tool-icon">
-            <img src="cut.png" alt="Cut tool" width="200px" height="220px">
-            <h2 class="tool-shortcut">Cut: Shift + C</h2>
-        </li>
-    </ul>
-
-    <ul>
-        <li class="information-tools">
+        </div>
+        <div class="tool-info">
             <h2>Brush Tool</h2>
             <p>Can be used to draw.</p>
-        </li>
-        <li class="information-tools">
+        </div>
+    </li>
+    <li>
+        <div class="tool-icon">
+            <img src="eraser.png" alt="Eraser tool" width="200px" height="220px">
+            <h2 class="tool-shortcut">Eraser: Shift + E</h2>
+        </div>
+        <div class="tool-info">
             <h2>Eraser Tool</h2>
             <p>Will erase parts of the drawing on the selected layer.</p>
-        </li>
-        <li class="information-tools">
+        </div>
+    </li>
+
+    <li>
+        <div class="tool-icon">
+            <img src="lasso.png" alt="Lasso tool" width="200px" height="220px">
+            <h2 class="tool-shortcut">Lasso: Shift + L</h2>
+        </div>
+        <div class="tool-info">
             <h2>Lasso Tool</h2>
             <p>Can be used to select parts of the drawing on a selected layer.</p>
-        </li>
-        <li class="information-tools">
+        </div>
+    </li>
+    <li>
+        <div class="tool-icon">
+            <img src="drag.png" alt="Drag tool" width="200px" height="220px">
+            <h2 class="tool-shortcut">Drag: Shift + D</h2>
+        </div>
+        <div class="tool-info">
             <h2>Drag Tool</h2>
             <p>Can be used to draw parts of the drawing around.</p>
-        </li>
-        <li class="information-tools">
+        </div>
+    </li>
+    <li>
+        <div class="tool-icon">
+            <img src="cut.png" alt="Cut tool" width="200px" height="220px">
+            <h2 class="tool-shortcut">Cut: Shift + C</h2>
+        </div>
+        <div class="tool-info">
             <h2>Cut Tool</h2>
             <p>Will cut out a part of the drawing after using the lasso tool.</p>
-        </li>
-    </ul>
+        </div>
+    </li>
+</ul>
 
 <br><br><br><br><br><br><br><br><br><br><br><br>
 <br><br><br><br><br><br><br><br><br><br><br><br>
