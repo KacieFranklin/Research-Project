8e4e7d4813ae889edd1802279a0b6fd82039b4f6
kacie franklin <C00299407@setu.ie>
2025-04-07 19:52:29 +0100

    Added responsiveness to Products page

--- svelte-app/src/lib/components/Footer.svelte
+++ svelte-app/src/lib/components/Footer.svelte
@@ -6,7 +6,7 @@
     <div style="position:fixed;bottom:45px;background-color:#446;z-index:8">
         <a href="https://svelte.dev/docs/kit">Svelte Documentation</a><br>
     </div>
-    <p>2025 Perseplex. All rights reserved</p>
+    <p>2025 Perseplax. All rights reserved</p>
 </footer>
 
 <style>
